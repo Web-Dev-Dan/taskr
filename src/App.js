4a99806bda15b7ea9b08b04e728a1663d4a83e21
Dan <webdevd4n@gmail.com>
2022-07-10 15:58:59 +0900

    Universal Styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
+import Navbar from "./components/navbar/Navbar";
+
 function App() {
   return (
     <div className="App">
-      Hello
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            nav
+        </nav>
+    )
+}
+
+export default Navbar;
